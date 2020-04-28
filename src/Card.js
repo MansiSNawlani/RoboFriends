bf947475abfd236079b45534e537fa7a20cebcb8
mansishankarnawlani@gmail.com <mansishankarnawlani@gmail.com>
2020-04-28 17:41:10 +0530

    Optimizing RFApp using map function

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({name,email,id})=>{
 
     return(   
         <div className=' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt="robofriend pic" src={`https://robohash.org/${id}`}/>
+            <img alt="robofriend pic" src={`https://robohash.org/${id}?200*200`}/>
             <h2>{name}</h2>
             <p>{email}</p>
         </div>
